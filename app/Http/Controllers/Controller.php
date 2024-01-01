547a55bd2fb3f43bfc99e59382f545ccc560ee81
nitesh350 <niteshshrestha4321@gmail.com>
2024-01-01 22:58:11 +0545

    created show login from

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,5 +8,11 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, ValidatesRequests, hastoken ;
+
+    public function showLoginForm()
+    {
+        // Your login form view logic goes here
+        return view('auth.login');
+    }
 }
